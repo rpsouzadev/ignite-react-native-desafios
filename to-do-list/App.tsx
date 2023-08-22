cfd4428bd889021f6c0afd4602f29752ef800644
Rafael Pereira <rpsouza.dev@gmail.com>
2023-08-22 10:02:10 -0300

    Add fonts Inter

---
+++ to-do-list/App.tsx
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_700Bold,
+} from '@expo-google-fonts/inter'
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_700Bold,
+  })
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
