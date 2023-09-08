21a6146baffc35d87f778479d619fea5e7fa0b54
Rafael Souza <rpsouza.dev@gmail.com>
2023-09-08 17:16:25 -0300

    Add font Nunito Sans

--- daily-diet/App.tsx
+++ daily-diet/App.tsx
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import {
+  NunitoSans_400Regular,
+  NunitoSans_700Bold,
+  useFonts,
+} from '@expo-google-fonts/nunito-sans'
+import theme from '@theme/index'
+import { ThemeProvider } from 'styled-components/native'
+import { Home } from '@screens/home'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [fonstsLoaded] = useFonts({
+    NunitoSans_400Regular,
+    NunitoSans_700Bold,
+  })
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+      <StatusBar style="dark" translucent backgroundColor="transparent" />
+    </ThemeProvider>
+  )
+}
