c9f4b4510f8d0342e8f83c3d5923404737c9bf7e
Rafael Souza <rpsouza.dev@gmail.com>
2023-09-08 16:04:54 -0300

    Created theme and types for theme and png

--- daily-diet/babel.config.js
+++ daily-diet/babel.config.js
@@ -1,20 +1,23 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
     plugin: [
-      ['module-resolver', {
-        root: ['./src'],
-        alias: {
-          '@assets': './src/assets',
-          '@components': './src/components',
-          '@routes': './src/routes',
-          '@screens': './src/screens',
-          '@storage': './src/storage',
-          '@theme': './src/theme',
-          '@utils': './src/utils',
-        }
-      }]
-    ]
-  };
-};
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@assets': './src/assets',
+            '@components': './src/components',
+            '@routes': './src/routes',
+            '@screens': './src/screens',
+            '@storage': './src/storage',
+            '@theme': './src/theme',
+            '@utils': './src/utils',
+          },
+        },
+      ],
+    ],
+  }
+}
---
+++ daily-diet/src/theme/index.ts
@@ -0,0 +1,33 @@
+export default {
+  COLORS: {
+    WHITE: '#FFFFFF',
+
+    RED_DARK: '#BF3B44',
+    RED_MID: '#F3BABD',
+    RED_LIGHT: '#F4E6E7',
+
+    GREEN_DARK: '#639339',
+    GREEN_MID: '#CBE4B4',
+    GREEN_LIGHT: '#E5F0DB',
+
+    GRAY_100: '#1B1D1E',
+    GRAY_200: '#333638',
+    GRAY_300: '#5C6265',
+    GRAY_400: '#B9BBBC',
+    GRAY_500: '#DDDEDF',
+    GRAY_600: '#EFF0F0',
+    GRAY_700: '#FAFAFA',
+  },
+  FONT_FAMILY: {
+    REGULAR: 'NunitoSans_400Regular',
+    BOLD: 'NunitoSans_700Regular',
+  },
+  FONT_SIZE: {
+    SM: 12,
+    MD: 14,
+    LG: 16,
+    XL: 18,
+    '2XL': 24,
+    '3XL': 32,
+  },
+} as const
