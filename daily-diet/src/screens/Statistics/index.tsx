009aa46a60ef45f0fae450d287a52ba6b4276a16
Rafael Souza <rpsouza.dev@gmail.com>
2023-09-12 15:23:22 -0300

    Stylized statistics screen

--- daily-diet/src/screens/Statistics/index.tsx
+++ daily-diet/src/screens/Statistics/index.tsx
@@ -5,6 +5,34 @@ export function Statistics() {
   return (
     <S.StatisticsContainer>
       <StatisticsHeader />
+
+      <S.ContentContainer>
+        <S.Title>Estatísticas gerais</S.Title>
+
+        <S.Box>
+          <S.TextNumber>22</S.TextNumber>
+          <S.Description>
+            melhor sequência de pratos dentro da dieta
+          </S.Description>
+        </S.Box>
+
+        <S.Box>
+          <S.TextNumber>109</S.TextNumber>
+          <S.Description>refeições registradas </S.Description>
+        </S.Box>
+
+        <S.BoxWrapper>
+          <S.BoxSmall type="GOOD">
+            <S.TextNumber>99</S.TextNumber>
+            <S.Description>refeições dentro da dieta</S.Description>
+          </S.BoxSmall>
+
+          <S.BoxSmall type="BAD">
+            <S.TextNumber>10</S.TextNumber>
+            <S.Description>refeições fora da dieta</S.Description>
+          </S.BoxSmall>
+        </S.BoxWrapper>
+      </S.ContentContainer>
     </S.StatisticsContainer>
   )
 }
--- daily-diet/src/screens/Statistics/styled.ts
+++ daily-diet/src/screens/Statistics/styled.ts
@@ -1,6 +1,67 @@
-import styled from 'styled-components/native'
+import { StatusTypeProps } from '@components/Info/styled'
+import styled, { css } from 'styled-components/native'
+
+type StatisticsStyleProps = {
+  type: StatusTypeProps
+}
 
 export const StatisticsContainer = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.COLORS.GRAY_700};
+`
+export const ContentContainer = styled.View`
+  flex: 1;
+  top: -30px;
+  padding: 24px;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_700};
+`
+export const Title = styled.Text`
+  margin-bottom: 20px;
+  text-align: center;
+  ${({ theme }) => css`
+    color: ${theme.COLORS.GRAY_100};
+    font-size: ${theme.FONT_SIZE.MD}px;
+    font-family: ${theme.FONT_FAMILY.BOLD};
+  `}
+`
+export const TextNumber = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.COLORS.GRAY_100};
+    font-size: ${theme.FONT_SIZE['2XL']}px;
+    font-family: ${theme.FONT_FAMILY.BOLD};
+  `}
+`
+export const Description = styled.Text`
+  margin-top: 10px;
+  text-align: center;
+  ${({ theme }) => css`
+    color: ${theme.COLORS.GRAY_200};
+    font-size: ${theme.FONT_SIZE.MD}px;
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+  `}
+`
+export const Box = styled.View`
+  width: 100%;
+  padding: 16px;
+  margin-bottom: 8px;
+  border-radius: 8px;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_600};
+`
+export const BoxSmall = styled.View<StatisticsStyleProps>`
+  width: 168px;
+  padding: 16px;
+  border-radius: 8px;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme, type }) =>
+    type === 'GOOD' ? theme.COLORS.GREEN_LIGHT : theme.COLORS.RED_LIGHT};
+`
+export const BoxWrapper = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
 `
