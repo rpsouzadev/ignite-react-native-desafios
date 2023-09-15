041f95515dfb0c36df56c9dacc44a3d1b26e07cf
Rafael Souza <rpsouza.dev@gmail.com>
2023-09-15 11:13:43 -0300

    Add react-hook-form and yup

--- daily-diet/src/screens/NewMeal/index.tsx
+++ daily-diet/src/screens/NewMeal/index.tsx
@@ -1,19 +1,89 @@
 import * as S from './styles'
 import { Input } from '@components/Input'
 import { Header } from '@components/Header'
+import { Controller, useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+import { Button } from '@components/Button'
+
+type FormDataProps = {
+  name: string
+  description: string
+  date: string
+  hour: string
+}
+
+const newMealSchema = yup.object({
+  name: yup.string().required('Nome obrigatório'),
+  description: yup.string().required('Descrição obrigatória'),
+  date: yup.string().required('Data obrigatória'),
+  hour: yup.string().required('Hora obrigatória'),
+})
 
 export function NewMeal() {
+  const { control, handleSubmit } = useForm<FormDataProps>({
+    resolver: yupResolver(newMealSchema),
+  })
+
+  function handleAddNewMeal(data: FormDataProps) {
+    console.log('data => ', data)
+  }
+
   return (
     <S.NewMealContainer>
       <Header title="Nova refeição" />
 
       <S.ContentContainer>
-        <Input title="Nome" size={48} />
-        <Input title="Descrição" size={120} textAlignVertical="top" />
+        <Controller
+          control={control}
+          name="name"
+          render={({ field: { onChange } }) => (
+            <Input title="Nome" size={48} onChangeText={onChange} />
+          )}
+        />
+        <Controller
+          control={control}
+          name="description"
+          render={({ field: { onChange } }) => (
+            <Input
+              title="Descrição"
+              size={120}
+              textAlignVertical="top"
+              onChangeText={onChange}
+            />
+          )}
+        />
         <S.InputWrapper>
-          <Input title="Data" size={48} flexNumber={1} />
-          <Input title="Hora" size={48} flexNumber={1} />
+          <Controller
+            control={control}
+            name="date"
+            render={({ field: { onChange } }) => (
+              <Input
+                title="Data"
+                size={48}
+                flexNumber={1}
+                onChangeText={onChange}
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name="hour"
+            render={({ field: { onChange } }) => (
+              <Input
+                title="Hora"
+                size={48}
+                flexNumber={1}
+                onChangeText={onChange}
+              />
+            )}
+          />
         </S.InputWrapper>
+
+        <Button
+          title="Cadastrar refeição"
+          onPress={handleSubmit(handleAddNewMeal)}
+        />
       </S.ContentContainer>
     </S.NewMealContainer>
   )
