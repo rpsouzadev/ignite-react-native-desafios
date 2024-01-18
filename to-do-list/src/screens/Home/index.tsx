f61ca3fb19700f25cfc63b689c3f80f0ed2b4bfb
Rafael Souza <rpsouza.dev@gmail.com>
2024-01-18 20:44:36 -0300

    Save tasks in storage

--- to-do-list/src/screens/Home/index.tsx
+++ to-do-list/src/screens/Home/index.tsx
@@ -7,6 +7,7 @@ import { Info } from '../../components/Info'
 import { TaskCard } from '../../components/TaskCard'
 import { TaskEmpty } from '../../components/TaskEmpty'
 import { useTask } from './useTask'
+import { Loading } from '../../components/Loading'
 
 export function Home() {
   const {
@@ -17,42 +18,49 @@ export function Home() {
     handleAddNewTask,
     handleRemoveTask,
     handleToggleCheck,
+    isLoadingTaskStorageData,
   } = useTask()
 
   return (
-    <S.Container>
-      <Header />
+    <>
+      {isLoadingTaskStorageData ? (
+        <Loading />
+      ) : (
+        <S.Container>
+          <Header />
 
-      <S.WrapContent>
-        <S.InputContainer>
-          <Input
-            value={newTask}
-            autoCorrect={false}
-            inputRef={newTaskInputRef}
-            onChangeText={(text) => saveText(text)}
-          />
-          <Button onPress={handleAddNewTask} />
-        </S.InputContainer>
+          <S.WrapContent>
+            <S.InputContainer>
+              <Input
+                value={newTask}
+                autoCorrect={false}
+                inputRef={newTaskInputRef}
+                onChangeText={(text) => saveText(text)}
+              />
+              <Button onPress={handleAddNewTask} />
+            </S.InputContainer>
 
-        <Info data={task} />
+            <Info data={task} />
 
-        <FlatList
-          data={task}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => (
-            <TaskCard
-              data={item}
-              toggleCheck={() => handleToggleCheck(item.id, item.isChecked)}
-              onRemove={() => handleRemoveTask(item.id)}
+            <FlatList
+              data={task}
+              keyExtractor={(item) => item.id}
+              renderItem={({ item }) => (
+                <TaskCard
+                  data={item}
+                  toggleCheck={() => handleToggleCheck(item.id, item.isChecked)}
+                  onRemove={() => handleRemoveTask(item.id)}
+                />
+              )}
+              contentContainerStyle={{
+                paddingBottom: 100,
+              }}
+              showsVerticalScrollIndicator={false}
+              ListEmptyComponent={() => <TaskEmpty />}
             />
-          )}
-          contentContainerStyle={{
-            paddingBottom: 100,
-          }}
-          showsVerticalScrollIndicator={false}
-          ListEmptyComponent={() => <TaskEmpty />}
-        />
-      </S.WrapContent>
-    </S.Container>
+          </S.WrapContent>
+        </S.Container>
+      )}
+    </>
   )
 }
--- to-do-list/src/screens/Home/useTask.ts
+++ to-do-list/src/screens/Home/useTask.ts
@@ -1,11 +1,16 @@
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { TaskProps } from './types'
 import { Alert, TextInput } from 'react-native'
 import { TaskTextStyleProps } from '../../components/TaskCard/styles'
+import {
+  getTasksToStorage,
+  saveTasksToStorage,
+} from '../../storage/tasksStorage'
 
 export function useTask() {
   const [newTask, setNewTask] = useState('')
   const [task, setTask] = useState<TaskProps[]>([])
+  const [isLoadingTaskStorageData, setIsLoadingTaskStorageData] = useState(true)
 
   const newTaskInputRef = useRef<TextInput>(null)
 
@@ -13,7 +18,7 @@ export function useTask() {
     setNewTask(text)
   }
 
-  function handleAddNewTask() {
+  async function handleAddNewTask() {
     try {
       if (!newTask.trim()) {
         return Alert.alert('Campo em branco.', 'Digite uma nova tarefa.')
@@ -25,25 +30,38 @@ export function useTask() {
         isChecked: 'noCHECK',
       }
 
-      setTask((oldTasks) => [...oldTasks, newTaskData])
+      const addTask = [...task, newTaskData]
+
+      setTask(addTask)
+      await saveTasksToStorage(addTask)
       newTaskInputRef.current?.blur()
       setNewTask('')
     } catch (error) {
       console.log('ADD TASK ERROR => ', error)
+      Alert.alert(
+        'Adicionar tarefa',
+        'Não foi possível adicionar tarefa, tenente novamente mais tarde.',
+      )
     }
   }
 
-  function handleToggleCheck(id: string, checkbox: TaskTextStyleProps) {
+  async function handleToggleCheck(id: string, checkbox: TaskTextStyleProps) {
     try {
-      const checkboxStatus = checkbox === 'isCHECK' ? 'noCHECK' : 'isCHECK'
+      const checkboxStatus: TaskTextStyleProps =
+        checkbox === 'isCHECK' ? 'noCHECK' : 'isCHECK'
 
-      setTask((oldTasks) =>
-        oldTasks.map((task) =>
-          task.id === id ? { ...task, isChecked: checkboxStatus } : task,
-        ),
+      const updatedTasks = task.map((task) =>
+        task.id === id ? { ...task, isChecked: checkboxStatus } : task,
       )
+
+      setTask(updatedTasks)
+      await saveTasksToStorage(updatedTasks)
     } catch (error) {
       console.log('MARK AS CHECKED ERROR => ', error)
+      Alert.alert(
+        'Marcar como feito',
+        'Não foi possível marcar tarefa como feita, tenente novamente mais tarde.',
+      )
     }
   }
 
@@ -51,8 +69,7 @@ export function useTask() {
     Alert.alert('Apagar', 'Desenha realmente apagar a tarefa?', [
       {
         text: 'Sim',
-        onPress: () =>
-          setTask((oldTasks) => oldTasks.filter((task) => task.id !== id)),
+        onPress: async () => await removeTask(id),
       },
       {
         text: 'Não',
@@ -60,6 +77,44 @@ export function useTask() {
       },
     ])
   }
+
+  async function removeTask(id: string) {
+    try {
+      const updatedTasks = task.filter((task) => task.id !== id)
+
+      setTask(updatedTasks)
+      await saveTasksToStorage(updatedTasks)
+    } catch (error) {
+      console.log('removeTask error => ', error)
+      Alert.alert(
+        'Remover tarefa',
+        'Não foi possível remover tarefa, tenente novamente mais tarde.',
+      )
+    }
+  }
+
+  async function tasksLoaded() {
+    try {
+      setIsLoadingTaskStorageData(true)
+      const taskStorege = await getTasksToStorage()
+
+      if (taskStorege.length > 0) {
+        setTask(taskStorege)
+      }
+    } catch (error) {
+      console.log('tasksLoaded error => ', error)
+      Alert.alert(
+        'Carregar tarefas',
+        'Não foi possível carregar as tarefas, tenente novamente mais tarde.',
+      )
+    } finally {
+      setIsLoadingTaskStorageData(false)
+    }
+  }
+
+  useEffect(() => {
+    tasksLoaded()
+  }, [])
 
   return {
     task,
@@ -69,5 +124,6 @@ export function useTask() {
     handleAddNewTask,
     handleRemoveTask,
     handleToggleCheck,
+    isLoadingTaskStorageData,
   }
 }
