4bdf0ff0983dadeb1af1854be8b2f8ffdb85fe7f
Rafael Souza <rpsouza.dev@gmail.com>
2024-03-11 11:12:51 -0300

    Add input search

--- marketspace/src/screens/app/Home/Home.tsx
+++ marketspace/src/screens/app/Home/Home.tsx
@@ -1,8 +1,13 @@
-import { ScrollView, Text } from 'native-base'
+import { HStack, ScrollView, Text, View, useTheme } from 'native-base'
 import { HomeHeader } from './components/HomeHeader'
 import { AdsInfo } from './components/AdsInfo'
+import { Input } from '@/components/Input/Input'
+import { MagnifyingGlass, Sliders } from 'phosphor-react-native'
+import { Pressable } from 'react-native'
 
 export function Home() {
+  const { colors } = useTheme()
+
   return (
     <ScrollView
       flexGrow={1}
@@ -17,6 +22,25 @@ export function Home() {
       </Text>
 
       <AdsInfo />
+
+      <Text color="gray.300" fontSize="sm" mt={8} mb={3}>
+        Compre produtos variados
+      </Text>
+
+      <Input
+        placeholder="Buscar anúncio"
+        rightElement={
+          <HStack pr={4} space={3}>
+            <MagnifyingGlass size={24} color={colors.gray[200]} />
+
+            <View borderWidth={1} borderColor="gray.400" />
+
+            <Pressable>
+              <Sliders size={24} color={colors.gray[200]} />
+            </Pressable>
+          </HStack>
+        }
+      />
     </ScrollView>
   )
 }
